c86468a0a38ec029646ff26700b7d12d9f13b6b5
harrytimbog <harriemannie@gmail.com>
2024-10-09 21:42:40 +0100

    HTTP Delete

--- WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -95,6 +95,22 @@ namespace WebDiaryAPI.Controllers
             return NoContent();
         }
 
+        // Delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiaryEntry(int id)
+        {
+            var diaryEntry = await _context.DiaryEntries.FindAsync(id);
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            _context.DiaryEntries.Remove(diaryEntry);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
         //Helper method to check if DiaryEntry exists
         private bool DiaryEntryExists(int id)
         {
